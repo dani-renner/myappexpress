f7f7aebb362125019ba1ef9c1773c3b6ac3e2588
Dani Renner <danijrenner@gmail.com>
2022-03-10 18:54:28 -0800

    add body parser & get & post requests for users

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 4041;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = [{name: 'Tony', email: 'tony@mail.com'}];
+
 app.get('/', (_, res) => {
   res.send('Your Express App');
+});
+
+app.get('/users', (_, res) => {
+  res.json({ ok: true, users });
+});
+
+app.get('/user/:name', (req, res) => {
+  const { name } = req.params;
+  const user = users.filter((user) => user.name === name)[0];
+  res.json({ ok: true, user });
+})
+
+app.post('/adduser', (req,res) => {
+  const { name, email } = req.body;
+  if (name && email) {
+    users.push({ name, email });
+    res.json({ok: true, users});
+  }
 });
 
 app.listen(port, () => {
